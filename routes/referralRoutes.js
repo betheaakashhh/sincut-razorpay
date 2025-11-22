1468ed9edfb9d4c9edaeb854537bc7be0acc5a24
Aakash sahu <sahu.aakash1008@gmail.com>
2025-11-23 02:15:03 +0530

    updated referral ROutes as rewardReferral is invalid

--- routes/referralRoutes.js
+++ routes/referralRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { rewardReferral } from "../controllers/referralController.js";
+import { rewardReferralPayment } from "../controllers/referralController.js";
 import {convertCoinsToDivine, useDivineCoin, getWallet} from "../controllers/walletController.js";
 import { getReferralDashboard } from "../controllers/referralController.js";
 const router = express.Router();
 
-router.post("/reward-payment", protect, rewardReferral);
+router.post("/reward-payment", protect, rewardReferralPayment);
 router.get("/referral/dashboard", protect, getReferralDashboard);
 router.post("/referral/convert", protect, convertCoinsToDivine);
 router.post("/referral/use-divine", protect, useDivineCoin);
